23ca4c2c08e3ec8207565b592da9d704353388de
Sam Pita <sampita@Sams-MacBook-Pro.local>
2020-10-03 19:17:33 -0500

    add docstrings

--- withyouapi/views/member.py
+++ withyouapi/views/member.py
@@ -1,3 +1,4 @@
+"""View module for handling requests about members"""
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
