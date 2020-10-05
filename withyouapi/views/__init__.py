ac50333498f75a081d802622c7a14205b2fdf471
fillerInk <thatonephysicsnerd@gmail.com>
2020-10-05 10:58:51 +0530

    Added City Viewset

--- withyouapi/views/__init__.py
+++ withyouapi/views/__init__.py
@@ -4,3 +4,7 @@ from .form import Forms
 from .user import Users
 from .member import Members
 from .memberType import MemberTypes
+from .city import Cities
+from .country import Countries
+from .clinic import Clinics
+from .clinicReview import ClinicReviews
---
+++ withyouapi/views/city.py
@@ -0,0 +1,40 @@
+"""View module for handling requests about cities"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
+from withyouapi.models import City
+
+
+class CitySerializer(serializers.HyperlinkedModelSerializer):
+    """JSON serializer for cities
+
+    Arguments:
+        serializers.HyperlinkedModelSerializer
+    """
+    class Meta:
+        model = City
+        url = serializers.HyperlinkedIdentityField(
+            view_name='city',
+            lookup_field='id'
+        )
+        fields = ('name')
+
+
+class Cities(ViewSet):
+    """Cities"""
+            
+    def list(self, request):
+        """Handle GET requests to cities resource
+
+        Returns:
+            Response -- JSON serialized list of cities
+        """
+        cities = City.objects.all()
+        serializer = CitySerializer(
+            cities,
+            many=True,
+            context={'request': request}
+        )
+        return Response(serializer.data)
